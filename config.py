07212bcabb13a9b3b7fcf4e76ad171eac3215bed
shainyguy <shainyguy@gmail.com>
2026-02-11 16:56:03 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# токены и ключи
+TELEGRAM_BOT_TOKEN = "твой_telegram_token"
+GIGACHAT_API_KEY = "твой_giga_api_key"
+YOOKASSA_SHOP_ID = "твой_shop_id"
+YOOKASSA_SECRET_KEY = "твой_secret_key"
+WB_CLIENT_ID = "твой_wb_client_id"
+WB_CLIENT_SECRET = "твой_wb_client_secret"
